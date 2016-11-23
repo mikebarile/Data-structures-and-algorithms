d12a486bff9345963eb72bb369b82b353c8d53c5
Mike Barile <mikebarile13@gmail.com>
2016-11-23 12:47:45 -0800

    Implemented pop and push

--- dynamicArray/lib/dynamic_array.rb
+++ dynamicArray/lib/dynamic_array.rb
@@ -3,24 +3,32 @@ require_relative "static_array"
 class DynamicArray
   attr_reader :length
 
-  def initialize
+  def initialize(capacity=0)
+    @length = 0
+    @capacity = capacity
+    @arr = StaticArray.new(@capacity)
   end
 
-  # O(1)
   def [](index)
+    @arr[index]
   end
 
-  # O(1)
   def []=(index, value)
+    @arr[index] = value
   end
 
-  # O(1)
   def pop
+    throw 'index out of bounds' if length == 0
+    @length -= 1
+    val = @arr[length]
+    @arr[length] = nil
+    val
   end
 
-  # O(1) ammortized; O(n) worst case. Variable because of the possible
-  # resize.
   def push(val)
+    resize! if length == capacity
+    @arr[@length] = val
+    @length += 1
   end
 
   # O(n): has to shift over all the elements.
@@ -40,5 +48,13 @@ class DynamicArray
 
   # O(n): has to copy over all the elements to the new store.
   def resize!
+    new_arr = @capacity == 0 ? StaticArray.new(1) : StaticArray.new(@capacity * 2)
+    i = 0
+    while i < length
+      new_arr[i] = @arr[i]
+      i += 1
+    end
+    @capacity = @capacity == 0 ? 1 : @capacity * 2
+    @arr = new_arr
   end
 end
--- dynamicArray/lib/static_array.rb
+++ dynamicArray/lib/static_array.rb
@@ -1,14 +1,24 @@
 # This class just dumbs down a regular Array to be staticly sized.
 class StaticArray
   def initialize(length)
+    @arr = []
+    @length = length
   end
 
   # O(1)
   def [](index)
+    if index >= @length
+      raise 'index out of bounds'
+    end
+    @arr[index]
   end
 
   # O(1)
   def []=(index, value)
+    if index >= @length
+      raise 'index out of bounds'
+    end
+    @arr[index] = value
   end
 
   protected
