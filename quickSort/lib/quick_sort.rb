2ed2e5957b2388ef3bed6ed4ce1fdfbf98eb4dc9
Mike Barile <mikebarile13@gmail.com>
2016-11-29 13:41:26 -0800

    Implemented quicksort

--- quickSort/lib/quick_sort.rb
+++ quickSort/lib/quick_sort.rb
@@ -1,19 +1,26 @@
 class QuickSort
 
-  # Not in-place. Uses O(n) memory.
-  def self.sort1(array)
-
-  end
-
-  # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
+    prc = prc || proc {|el1, el2| el1 <=> el2}
 
+    return array if length < 2
+
+    new_idx = partition(array, start, length, &prc)
+
+    left_length = new_idx - start
+    right_length = length - (left_length + 1)
+    sort2!(array, start, left_length, &prc)
+    sort2!(array, new_idx + 1, right_length, &prc)
+
+    array
   end
+
+  protected
 
   def self.partition(array, start, length, &prc)
     prc = prc || proc {|el1, el2| el1 <=> el2}
 
-    (start + 1 .. start + length).each do |idx|
+    (start + 1 ... start + length).each do |idx|
       if prc.call(array[start], array[idx]) == 1
         array[start+1], array[idx] = array[idx], array[start+1]
         array[start], array[start+1] = array[start+1], array[start]
