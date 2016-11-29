05e1e96675e2ef7876360b56acb6d4a14c9bc1ff
Mike Barile <mikebarile13@gmail.com>
2016-11-28 21:22:51 -0800

    Finished heapsort

--- heap/lib/heap_sort.rb
+++ heap/lib/heap_sort.rb
@@ -2,5 +2,15 @@ require_relative "heap"
 
 class Array
   def heap_sort!
+    (2..self.length).each do |heap_sec|
+      BinaryMinHeap.heapify_up(self, heap_sec - 1, heap_sec)
+    end
+
+    self.length.downto(2).each do |heap_sec|
+      self[heap_sec - 1], self[0] = self[0], self[heap_sec - 1]
+      BinaryMinHeap.heapify_down(self, 0, heap_sec - 1)
+    end
+
+    self.reverse!
   end
 end
