54aa9900f13255ee14d1130a105953e333db7a85
Mike Barile <mikebarile13@gmail.com>
2016-11-29 13:19:49 -0800

    Succesfully implemented partition method

--- quickSort/lib/quick_sort.rb
+++ quickSort/lib/quick_sort.rb
@@ -1,15 +1,26 @@
 class QuickSort
-  # Quick sort has average case time complexity O(nlogn), but worst
-  # case O(n**2).
 
   # Not in-place. Uses O(n) memory.
   def self.sort1(array)
+
   end
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
+
   end
 
   def self.partition(array, start, length, &prc)
+    prc = prc || proc {|el1, el2| el1 <=> el2}
+
+    (start + 1 .. start + length).each do |idx|
+      if prc.call(array[start], array[idx]) == 1
+        array[start+1], array[idx] = array[idx], array[start+1]
+        array[start], array[start+1] = array[start+1], array[start]
+        start += 1
+      end
+    end
+
+    start
   end
 end
