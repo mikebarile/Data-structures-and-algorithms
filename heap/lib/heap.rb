63741a98e542974f82be8e219e4fc344edd0e9d3
Mike Barile <mikebarile13@gmail.com>
2016-11-28 20:48:31 -0800

    Debugged heapify up and down

--- heap/lib/heap.rb
+++ heap/lib/heap.rb
@@ -1,5 +1,6 @@
 class BinaryMinHeap
   def initialize(&prc)
+    @prc = prc || Proc.new { |el1, el2| el1 <=> el2 }
     @store = []
   end
 
@@ -16,7 +17,7 @@ class BinaryMinHeap
   end
 
   protected
-  attr_accessor :prc, :store
+  attr_accessor :store
 
   public
   def self.child_indices(len, parent_index)
@@ -30,13 +31,61 @@ class BinaryMinHeap
     if child_index == 0
       raise 'root has no parent'
     end
-    
+
     (child_index - 1) / 2
   end
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
+    prc ||= Proc.new { |el1, el2| el1 <=> el2 }
+
+    child1, child2 = child_indices(len, parent_idx)
+    children = []
+    children << array[child1] if array[child1]
+    children << array[child2] if array[child2]
+    if children.all? { |child| prc.call(array[parent_idx], child) <= 0}
+      return array
+    end
+
+    i = parent_idx
+    while i <= parent_index(len - 1)
+      child1, child2 = child_indices(len, i)
+      children = []
+      children << array[child1] if array[child1]
+      children << array[child2] if array[child2]
+      if children.length == 1
+        smallest = child1
+      elsif children.length == 0
+        break
+      else
+        smallest = prc.call(child1, child2) == -1 ? child1 : child2
+      end
+
+      if prc.call(array[i], array[smallest]) >= 0
+        swap!(array, i, smallest)
+        i = smallest
+      else
+        break
+      end
+    end
+    heapify_down(array, parent_idx, len, &prc)
   end
 
   def self.heapify_up(array, child_idx, len = array.length, &prc)
+    prc ||= Proc.new { |el1, el2| el1 <=> el2 }
+    i = child_idx
+    while i >= 1
+      parent = parent_index(i)
+      if prc.call(array[i], array[parent]) <= 0
+        swap!(array, i, parent)
+        i = parent
+      else
+        break
+      end
+    end
+    return array
+  end
+
+  def self.swap!(array, idx1, idx2)
+    array[idx1], array[idx2] = array[idx2], array[idx1]
   end
 end
